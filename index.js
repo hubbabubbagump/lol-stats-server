2f271ac701a2be11e7faa3ec4091c7358745937e
Ethan Jung <minibot4000@gmail.com>
2019-02-08 02:37:40 -0800

    Added ACAO

--- index.js
+++ index.js
@@ -136,6 +136,7 @@ app.get('/summoner', (req, res) => {
                      }
                   });
 
+                  res.header("Access-Control-Allow-Origin", "*");
                   res.send(response);
                }
                else {
